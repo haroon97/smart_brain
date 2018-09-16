ded6a0ca0dbd9a1a1a02ab0700bdd29185fc2e37
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-17 01:38:47 +0400

    added navigation component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello React</h1>
+        <Navigation />
       </div>
     );
   }
