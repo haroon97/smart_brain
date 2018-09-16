6f8f415270555016d4a33b135c6af4d1ae03abed
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-17 03:03:04 +0400

    added the rank component and used react-particles component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation.js';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 
+const particlesOption = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles 
+         params={particlesOption}
+         className='particles'
+      />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div className='white f3'>
+      <div className='center'>
+        {'Haroon, your current rank is...'}
+      </div>
+      <div className='center'>
+        {'#5'}
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
