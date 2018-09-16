2ec469016a6bcd8368f4042f0a85d1a70e8af21d
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-17 02:16:47 +0400

    added the logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation.js';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Logo />
+        <ImageLinkForm />
       </div>
     );
   }
---
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <h1>Image Link Form</h1>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
