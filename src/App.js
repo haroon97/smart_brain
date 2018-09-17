9586aa4325c2b41923048f82c3a1c2852d3269d6
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-17 04:55:29 +0400

    connected with the clarifai api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
 import Particles from 'react-particles-js';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
+import Clarifai from 'clarifai';
 
 const particlesOption = {
   particles: {
@@ -16,7 +18,35 @@ const particlesOption = {
     }
   }
 }
+
+const app = new Clarifai.App({
+  apiKey: 'b9f7c6ccb3e74b07a9a10683a15fb4d6'
+ });
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onSubmit = () => {
+    this.setState( {imageUrl: this.state.input} )
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+    }
+
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
@@ -27,7 +57,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './imageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = (props) => {
   return (
     <div>
       <p className='f3 center'>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='pa4 br3 shadow-5 center form'>
-          <input type='text' className='f4 pa2 w-70 center'/>
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple button' >Detect</button>
+          <input type='text' className='f4 pa2 w-70 center' onChange={props.onInputChange}/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple button' onClick={props.onSubmit}>Detect</button>
         </div>
       </div>
     </div>
